c7315854b37c73cc783171fca2699618c75c525e
yukiho <is0436er@ed.ritsumei.ac.jp>
2018-10-31 10:45:41 +0900

    [add] kotlinプロジェクトを追加

---
+++ kotlinSample/src/main/kotlin/jp/ac/ritsumei/rcc/sample/SampleApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.ritsumei.rcc.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleApplication>(*args)
+}
